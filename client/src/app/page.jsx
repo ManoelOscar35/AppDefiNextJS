842df7599c3163e77a5eb815e427988c2296c12a
ManoelOscar35 <manoeloscar557@gmail.com>
2024-09-10 19:28:26 -0300

    criando header e fazendo importações

--- client/src/app/page.jsx
+++ client/src/app/page.jsx
@@ -1,3 +1,5 @@
+import Header from '../components/Header';
+
 const style = {
   wrapper: 'h-screen max-h-screen h-min-screen w-screen bg-[#2D242F] text-white select-none flex flex-col justify-between '
 }
@@ -5,9 +7,9 @@ const style = {
 export default function Home() {
   return (
     <div className={style.wrapper}>
-      <h2>Olá!</h2>
-      <h2>Até logo!</h2>
-      <h2>Vamos codar!</h2>
+      <Header />
+      <h2>Main</h2>
+      <h2>Transaction History</h2>
     </div>
   );
 }
---
+++ client/src/components/Header.js
@@ -0,0 +1,17 @@
+import React, {useState, useEffect} from 'react';
+import { FiArrowUpRight } from 'react-icons/fi';
+import { AiOutlineDown } from 'react-icons/ai';
+import { HiOutlineDotsVertical } from 'react-icons/hi';
+
+const Header = () => {
+  return (
+    <header>
+        <p>Header</p>
+        <nav>
+        
+        </nav>
+    </header>
+  )
+}
+
+export default Header;
